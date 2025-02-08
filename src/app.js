85e90144b5379c487170c970777938b98e565131
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2025-02-09 01:52:01 +0530

    feat: add custom api response and error handling

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// limit the json data to prevent server crash
+app.use(express.json({ limit: "16kb" }));
+
+// Middleware to parse URL-encoded data from incoming requests
+// - `extended: true` allows parsing of complex objects and arrays
+// - `limit: "16kb"` restricts the request body size to 16KB to prevent large payloads
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Storing files in 'public' directory, files can be access directly
+app.use(express.static("public"));
+
+// TO perfrom CRUD over cookies on browser
+app.use(cookieParser());
+
+export { app };
